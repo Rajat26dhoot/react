4f72e74c4b4c434233e15bbf9e330bb3d9a200c6
Rajat26dhoot <2rajatd6@gmail.com>
2025-10-25 19:25:53 +0530

    new update

--- src/components/Auth/GoogleLoginButton.jsx
+++ src/components/Auth/GoogleLoginButton.jsx
@@ -1,8 +1,17 @@
 import React, { forwardRef } from 'react';
 
 const GoogleLoginButton = forwardRef((_, ref) => (
-  <div className="flex justify-center">
-    <div ref={ref} id="googleSignInButton" style={{ width: '100%' }}></div>
+  <div className="flex justify-center w-full">
+    <div
+      ref={ref}
+      id="googleSignInButton"
+      style={{
+        width: '100%',
+        minWidth: '100%', 
+        flexShrink: 0,     
+        boxSizing: 'border-box',
+      }}
+    ></div>
   </div>
 ));
 
